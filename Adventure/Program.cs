d87f2db466221cbba37ef317eef1d71bea839827
Jonikiro <mcabrera122@gmail.com>
2018-03-15 13:45:46 -0500

    First Commit

---
+++ Adventure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adventure
+{
+    class Program
+    {
+        static void Main()
+        {
+            string charName = "John Doe"; 
+            Console.WriteLine("Welcome to the magical realm of Daz!");
+            Console.Write("What is your name, traveler? ");
+            charName = Console.ReadLine();
+            Console.WriteLine("Dare you enter my magical realm, " + charName + "?");
+            Console.Read();
+        }
+    }
+}
