f6b3625799aec2e9f9d3e2b8a9069b97841a7df9
Jonikiro <mcabrera122@gmail.com>
2018-03-16 14:05:04 -0500

    Added multi class functionality

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*An adventure game by Matthew Cabrera
+* Development began March 15, 2018
+*
+* This work is a derivative of
+* "C# Adventure Game" by http://programmingisfun.com, used under CC BY.
+* https://creativecommons.org/licenses/by/4.0/
+*/
+
+using System;
 
 namespace Adventure
 {
@@ -6,12 +14,55 @@ namespace Adventure
     {
         static void Main()
         {
-            string charName = "John Doe"; 
-            Console.WriteLine("Welcome to the magical realm of Daz!");
-            Console.Write("What is your name, traveler? ");
-            charName = Console.ReadLine();
-            Console.WriteLine("Dare you enter my magical realm, " + charName + "?");
-            Console.Read();
+            Game.StartGame();
+            Console.ReadKey();
         }
+    }
+
+    public static class Game
+    {
+        static string charName = "John Doe";
+
+        public static void StartGame()
+        {
+            Console.Title = "Adventure Game";
+            Console.WriteLine("Matt's Adventure Game!");
+            Dialog("Welcome to the magical realm of Daz!");
+            NameCharacter();
+        }
+        
+        static void NameCharacter()
+        {
+            Dialog("What is your name, traveler? ");
+            charName = Console.ReadLine();
+            Dialog("Dare you enter my magical realm, " + charName + "?");
+        }
+
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void Dialog(string message, string color)
+        {
+            if (color == "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (color == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (color == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+
+    class Items
+    {
+
     }
 }
