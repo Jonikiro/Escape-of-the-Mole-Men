51df9a90a2712e38915048820fd03b3ad3549e9d
Jonikiro <mcabrera122@gmail.com>
2018-03-20 14:59:15 -0500

    Built more content for Intro

--- Adventure/Intro.cs
+++ Adventure/Intro.cs
@@ -14,22 +14,29 @@ namespace Adventure
             Console.Title = "Escape of the Mole Men";
 
             // Initiates first action of the game
-            Console.WriteLine("(Type \"LIGHT\" to turn your flashlight on) ");
-            while (Console.ReadLine().ToUpper() != "LIGHT")
-            {
-                Console.Clear();
-                Console.WriteLine("(Type \"LIGHT\" to turn your flashlight on) ", "w");
-            }
-            Console.Clear();
+            Program.Action("(Type \"LIGHT\" to turn your flashlight on)\n\nEnter your command: ", "LIGHT");
 
-            // Dialog 1
+            // Intro Dialog 1
             Program.Dialog("You breathe a sigh of relief as a beam of light cuts through the darkness.\n" +
-                "Tonight's desperate plan will be hard enough as it is, but it would be just\n" +
-                "your luck if something went wrong right out of the gate.", "w");
-            Program.Dialog("\"Hey Frankie, did you see something over there?\"", "red");
-            Program.Dialog("Shit.", "w");
+                "Getting your electronics from the Vegas City Dump had always been a crapshoot, \n" +
+                "but if tonight went smoothly, you'd never have to go back there again.", "w");
 
+            // Intro Action 1
+            Program.Action("Time to get a good look at this place and SHINE your light ahead.\n\nEnter your command: ", "SHINE");
 
+            // Intro Dialog 2
+            Program.Dialog("You pass the light over the building in front of you and see...\n" +
+                "A real dump. Graffiti on the piss-stained brick walls, shattered glass behind\n" +
+                "window bars. No indication that this abandoned apartment was anything more than\n" +
+                "a squatters den.", "w");
+            Program.Dialog("Nothing, except that Two-Tooth Tom swore he saw a convoy of real sharp-\n" +
+                "looking government types wheeling a bunch of fancy equipment into the building.\n" +
+                "That meant something valuable was in there.", "w");
+
+            // Intro Action 3
+            Program.Action("\"Time to get to work,\" you say, pulling out your LOCKPICK set.\n\nEnter your command: ", "LOCKPICK");
+
+            Program.Dialog("END OF CURRENT VERSION", "w");
         }
     }
 }
--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -2,8 +2,8 @@
 * Development began March 15, 2018
 *
 * This work is a derivative of
-* "C# Adventure Game" by http://Progammingisfun.com, used under CC BY.
-* https://creativecommons.org/licenses/by/4.0/
+* "C# Adventure Game" by http:// Progammingisfun.com, used under CC BY.
+* https:// creativecommons.org/licenses/by/4.0/
 */
 
 using System;
@@ -16,14 +16,14 @@ namespace Adventure
 {
     class Program
     {
-        //Main game loop
+        // Main game loop
         static void Main()
         {
             Intro.IntroMain();
             Console.ReadKey();
         }
 
-        //Allows the user to continue at their own pace.
+        // Allows the user to continue at their own pace.
         public static void Cont()
         {
             Console.WriteLine("\nPress the spacebar to continue...");
@@ -32,7 +32,7 @@ namespace Adventure
             Console.Clear();
         }
 
-        //Allows for customizable text to differentiate actors.
+        // Allows for customizable text to differentiate actors.
         public static void Dialog(string message, string color)
         {
             if (color == "red")
@@ -41,12 +41,27 @@ namespace Adventure
             { Console.ForegroundColor = ConsoleColor.Green; }
             else if (color == "yellow")
             { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else if (color == "purple")
+            { Console.ForegroundColor = ConsoleColor.Magenta; }
             else
             { Console.ForegroundColor = ConsoleColor.White; }
 
             Console.WriteLine(message);
             Console.ResetColor();
             Cont();
+        }
+
+        public static void Action(string message, string action)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(message);
+            while (Console.ReadLine().ToUpper().Replace(" ", "") != action)
+            {
+                Console.Clear();
+                Console.Write(message);
+            }
+            Console.Clear();
+            Console.ResetColor();
         }
     }
 }
